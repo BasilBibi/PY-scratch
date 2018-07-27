5a478d1e0fb9f1bf1737112da8f6a8c626dc137e
Basil <basilbibi@hotmail.com>
2018-07-27 06:11:31 +0200

    Refactor as GameBoard class

--- py_scratch/practise_python/GameBoard.py
+++ py_scratch/practise_python/GameBoard.py
@@ -1,10 +1,22 @@
-def row(line):
-    return f'| {line[0]} | {line[1]} | {line[2]} |\n{separator()}'
+class GameBoard:
 
+    def __init__(self, size, init_char=' '):
+        self.size = size
+        self.places = [[init_char] * size] * size
 
-def separator():
-    return f' --- --- --- '
+    def row_str(self, line):
+        l = [self.cell_str(e) for e in line]
+        return ''.join(l) + '|' + '\n' + self.separator_str()
 
+    def cell_str(self, e):
+        return f'| {e} '
 
-def make_gameboard(l):
-    return separator()+'\n'+'\n'.join( [row(e) for e in l] )
+    def separator_str(self):
+        return ' ---' * self.size
+
+    def get_current_board(self):
+        board = [self.row_str(line) for line in self.places]
+        return '\n' \
+               + self.separator_str()+'\n' \
+               + '\n'.join(board)\
+               + '\n'
--- test/practise_python/test_GameBoard.py
+++ test/practise_python/test_GameBoard.py
@@ -1,29 +1,52 @@
 import unittest
-from py_scratch.practise_python.GameBoard import separator, row, make_gameboard
+from py_scratch.practise_python.GameBoard import GameBoard
 
 
-class RPSTests(unittest.TestCase):
+class GameBoardTests(unittest.TestCase):
 
-    def test_separator(self):
-        self.assertEqual(' --- --- --- ', separator())
+    def test_1x1_construction(self):
+        g1x1 = GameBoard(1, 'X')
+        self.assertEqual(1, g1x1.size)
+        self.assertEqual([['X']], g1x1.places)
 
-    def test_row(self):
+    def test_3x3_construction(self):
+        g3x3 = GameBoard(3, 'X')
+        self.assertEqual(3, g3x3.size)
+        self.assertEqual([['X', 'X', 'X'],
+                          ['X', 'X', 'X'],
+                          ['X', 'X', 'X']], g3x3.places)
+
+    def test_separator_str(self):
+        g3x3 = GameBoard(3, 'X')
+        self.assertEqual(' --- --- ---', g3x3.separator_str())
+
+    def test_cell_str(self):
+        g1x1 = GameBoard(1, 'X')
+        self.assertEqual('| X ', g1x1.cell_str('X'))
+
+    def test_row_str(self):
+        g3x3 = GameBoard(3, 'X')
         line = ['X', 'X', 'X']
-        self.assertEqual('| X | X | X |\n --- --- --- ', row(line))
+        self.assertEqual('''| X | X | X |
+ --- --- ---''', g3x3.row_str(line))
 
-    def test_gameboard(self):
-        game = [ ['X', 'X', 'X'],
-                 ['X', 'X', 'X'],
-                 ['X', 'X', 'X']
-        ]
-        self.assertEqual(
-''' --- --- --- 
+    def test_get_current_board(self):
+
+        g3x3 = GameBoard(3, 'X')
+
+        game = [['X', 'X', 'X'],
+                ['X', 'X', 'X'],
+                ['X', 'X', 'X']]
+
+        self.assertEqual('''
+ --- --- ---
 | X | X | X |
- --- --- --- 
+ --- --- ---
 | X | X | X |
- --- --- --- 
+ --- --- ---
 | X | X | X |
- --- --- --- ''', make_gameboard(game))
+ --- --- ---
+''', g3x3.get_current_board())
 
 
 if __name__ == '__main__':
