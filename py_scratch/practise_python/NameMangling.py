38c5b1300ac78c0342e2b1ee6110864515acead3
Basil <basilbibi@hotmail.com>
2018-09-12 05:00:48 +0100

    Refactored __ name mangling tests

--- py_scratch/practise_python/NameMangling.py
+++ py_scratch/practise_python/NameMangling.py
@@ -1,19 +1,20 @@
 class A:
-    __clazz_var = 10
+    clazz_var = 1
+    _priv_clazz_var = 10
+    __mangled_clazz_var = 100
 
     def __init__(self, iv):
-        self.__inst_var = iv
-
-    def get_inst_var(self):
-        return self.__inst_var
-
+        self.inst_var = iv
+        self._priv_inst_var = iv
+        self.__mangled_inst_var = iv
 
 class B(A):
-    __another_clazz_var = 100
+    another_clazz_var = 100
+    _another_priv_clazz_var = 100
+    __another_mangled_clazz_var = 100
 
     def __init__(self, iv, aiv):
         A.__init__(self, iv)
-        self.__another_inst_var = aiv
-
-    def get_another_inst_var(self):
-        return self.__another_inst_var
+        self.another_inst_var = aiv
+        self._another_priv_inst_var = aiv
+        self.__another_mangled_inst_var = aiv
--- test/udemy_oo/section3/test_FilteredSet.py
+++ test/udemy_oo/section3/test_FilteredSet.py
@@ -11,40 +11,40 @@ class FilteredSetTests(unittest.TestCase):
         self.assertTrue(fs.get_set() == set())
 
     def test_evens_only_set_can_add_2(self):
-        fs = FilteredSet(lambda e: e % 2 == 0)
-        fs.add(2)
-        self.assertTrue( 2 in fs.get_set() )
+        even_ints = FilteredSet(lambda e: e % 2 == 0)
+        even_ints.add(2)
+        self.assertTrue( 2 in even_ints.get_set() )
 
     def test_evens_only_set_cannot_add_3(self):
-        fs = FilteredSet(lambda e: e % 2 == 0)
-        fs.add(3)
-        self.assertFalse( 3 in fs.get_set() )
+        even_ints = FilteredSet(lambda e: e % 2 == 0)
+        even_ints.add(3)
+        self.assertFalse( 3 in even_ints.get_set() )
 
     def test_internal_set_is_encapsulated(self):
-        fs = FilteredSet(lambda e: e % 2 == 0)
-        fs.add(2)
-        copy = fs.get_set()
+        even_ints = FilteredSet(lambda e: e % 2 == 0)
+        even_ints.add(2)
+        copy = even_ints.get_set()
         copy.add(15)
-        self.assertFalse( 15 in fs.get_set())
+        self.assertFalse( 15 in even_ints.get_set())
 
     def test_odds_only_set_can_add_2(self):
-        fs = FilteredSet(lambda e: e % 2 != 0)
-        fs.add(2)
-        self.assertFalse(2 in fs.get_set())
+        odd_ints = FilteredSet(lambda e: e % 2 != 0)
+        odd_ints.add(2)
+        self.assertFalse(2 in odd_ints.get_set())
 
     def test_bounds_set_can_add_in_range(self):
-        fs = FilteredSet(lambda e: 3 <= e <= 5)
-        fs.add(3)
-        fs.add(5)
-        self.assertTrue(3 in fs.get_set())
-        self.assertTrue(5 in fs.get_set())
+        odd_ints = FilteredSet(lambda e: 3 <= e <= 5)
+        odd_ints.add(3)
+        odd_ints.add(5)
+        self.assertTrue(3 in odd_ints.get_set())
+        self.assertTrue(5 in odd_ints.get_set())
 
     def test_bounds_set_cannot_add_out_of_range(self):
-        fs = FilteredSet(lambda e: 3 <= e <= 5)
-        fs.add(2)
-        fs.add(10)
-        self.assertFalse(2 in fs.get_set())
-        self.assertFalse(10 in fs.get_set())
+        bounded_ints_3_5 = FilteredSet(lambda e: 3 <= e <= 5)
+        bounded_ints_3_5.add(2)
+        bounded_ints_3_5.add(10)
+        self.assertFalse(2 in bounded_ints_3_5.get_set())
+        self.assertFalse(10 in bounded_ints_3_5.get_set())
 
 
 if __name__ == '__main__':
