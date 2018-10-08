e6727895cd1a3aac44d5f5da8a400860ae91c594
Basil <basilbibi@hotmail.com>
2018-10-08 21:43:11 +0100

    Refactor OxfordOnlineWordDictionary to use jsonpath

--- py_scratch/practise_python/WordDictionary.py
+++ py_scratch/practise_python/WordDictionary.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 import requests
-import json
+from jsonpath_ng import parse
 
 
 class WordDictionary(ABC):
@@ -10,8 +10,6 @@ class WordDictionary(ABC):
 
 
 class OxfordOnlineWordDictionary(WordDictionary):
-    # for more information on how to install requests
-    # http://docs.python-requests.org/en/master/user/install/#install
     _app_id = '9286dd2e'
     _app_key = '8651b0907938b33bb76e99cb157288d2'
     _language = 'en'
@@ -19,23 +17,43 @@ class OxfordOnlineWordDictionary(WordDictionary):
     _success = 200
     _word_not_found = 404
 
+    __DEF_PATH = parse('$..definitions')
+    __ETYM_PATH = parse('$..etymologies')
+
     def lookup(self, word):
         url = f'{OxfordOnlineWordDictionary._base_url}/{word.lower()}'
         r = requests.get(url, headers={'app_id': OxfordOnlineWordDictionary._app_id, 'app_key': OxfordOnlineWordDictionary._app_key})
 
         if r.status_code == self._success:
-            return self._extract_definition_from_result(r.text)
+            return self._extract_results(r.text)
         elif r.status_code == self._word_not_found:
             return [f'Oxford Online Dictionary could not find {word}']
         else:
             return ['Oxford Online Dictionary did not respond']
 
     @staticmethod
-    def _extract_definition_from_result(lookup_result):
-        j = json.loads(lookup_result)
-        senses = j['results'][0]['lexicalEntries'][0]['entries'][0]['senses']
-        definitions = [f'{i+1} : {sense["short_definitions"][0]}' for i, sense in enumerate(senses)]
-        return definitions
+    def _extract_results(lookup_result):
+        definitions = OxfordOnlineWordDictionary._extract_definitions(lookup_result)
+        etymologies = OxfordOnlineWordDictionary._extract_etymologies(lookup_result)
+        return [f'{i+1} : {definition}'
+                for i, definition in enumerate(etymologies + definitions)]
+
+    @staticmethod
+    def _extract_definitions(lookup_result):
+        definition_matches = [match.value for match in
+                              OxfordOnlineWordDictionary.__DEF_PATH.find(lookup_result)]
+        return [match for match
+                in OxfordOnlineWordDictionary._flatten_results(definition_matches)]
+
+    @staticmethod
+    def _extract_etymologies(lookup_result):
+        etymology_matches = [match.value for match in OxfordOnlineWordDictionary.__ETYM_PATH.find(lookup_result)]
+        return [match for match
+                in OxfordOnlineWordDictionary._flatten_results(etymology_matches)]
+
+    @staticmethod
+    def _flatten_results(list_of_lists):
+        return [item for sublist in list_of_lists for item in sublist]
 
 
 class LocalWordDictionary(WordDictionary):
--- test/practise_python/test_DictionaryAndThesurusTests.py
+++ test/practise_python/test_DictionaryAndThesurusTests.py
@@ -1,17 +1,22 @@
 import unittest
+import json
 from py_scratch.practise_python.WordDictionary import *
 from test.TestBase import *
 
-onlineoxford_pear_json = get_file_contents('Oxford_Pear.json')
+onlineoxford_pear = get_file_contents('Oxford_Pear.json')
+pear_json = json.loads(onlineoxford_pear)
 
 
 class HangmanTests(unittest.TestCase):
 
     def test_OnlineOxford_can_parse_its_own_json(self):
         oxford = OxfordOnlineWordDictionary()
-        definitions = oxford._extract_definition_from_result(onlineoxford_pear_json)
-        self.assertEqual( ["1 : sweet yellowish- or brownish-green edible fruit which is narrow at stalk",
-                           "2 : Eurasian tree which bears pear"], definitions )
+        result =  oxford._extract_results(pear_json)
+        self.assertEqual( ['1 : Old English pere, peru, of West Germanic origin; related to Dutch peer, '
+                           'from Latin pirum',
+                           '2 : a sweet yellowish- or brownish-green edible fruit that is typically '
+                               'narrow at the stalk and wider towards the base, with sweet, slightly gritty flesh.',
+                           '3 : the Eurasian tree which bears the pear.'], result )
 
     def test_LocalDictionary_making_internal_dict(self):
         internal_dict = LocalWordDictionary._make_internal_dict('LocalDictionary.csv')
