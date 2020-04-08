33fc8b6e72bbd4b1af12f66361eeea1ebecd4034
Basil <basilbibi@hotmail.com>
2020-04-08 12:51:40 +0100

    Added Bill to notifier to travis.

--- test/practise_python/test_LowercaseKeysOnlyDict.py
+++ test/practise_python/test_LowercaseKeysOnlyDict.py
@@ -1,21 +1,21 @@
 import pytest
-from py_scratch.practise_python.LowercaseKeysOnlyDict import SetItemError, LowercaseKeysOnlyDict
-
-
-def test_can_construct():
-    d = LowercaseKeysOnlyDict()
-    with pytest.raises(KeyError):
-        d['a']
+from py_scratch.practise_python.LowercaseKeysOnlyDict import LowercaseKeysOnlyDict, SetItemError
 
 
 def test_set_a_new_key_lowercase():
     d = LowercaseKeysOnlyDict()
     d['z'] = 10
-    assert 10 == d['z']
+    assert d['z'] == 10
 
 
 def test_set_a_new_key_uppercase_raises_error():
-    d = LowercaseKeysOnlyDict()
     with pytest.raises(SetItemError):
+        d = LowercaseKeysOnlyDict()
         d['Z'] = 10
 
+
+def test_can_construct_with_values():
+
+    d = dict( [('a', 1), ('b', 2)] )
+    d = LowercaseKeysOnlyDict( [('a', 123), ('b', 222)] )
+    assert d['a'] == 123
